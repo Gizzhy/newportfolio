536022155b8f8e1ef0a4c14e33596b7eb4845be9
Gizzhy <gizzhyakinz@gmail.com>
2023-03-17 22:14:59 +0100

    new3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ export const Navbar = () => {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link about" aria-current="page" href="hhtps://google.com">About</a>
+                <a class="nav-link about" aria-current="page" href="#about">About</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link service" aria-current="page" href="hhtps://google.com">Services</a>
--- src/pages/Body.js
+++ src/pages/Body.js
@@ -22,7 +22,7 @@ function Body() {
                                 <span>
                            <Typewriter
                            words={['A DIGITAL ENTHUSIAST', 'A FRONTEND WEB DEVELOPER']}
-                           loop={10}
+                           loop={0}
                            cursor
                            cursorStyle='|'
                                              typeSpeed={60}
@@ -38,6 +38,47 @@ function Body() {
                     </div>
                 </div>
         </section>
+        <div className="container-fluid lineplace">
+            <div className="row">
+                <div className="col-md-12">
+                    <hr className="line"/>
+                </div>
+            </div>
+        </div>
+        <section id="about">
+            <div className="container-fluid about-div">
+                <div className="row">
+                    <div className="col-md-12">
+                        <p className="aboutme">ABOUT ME</p>
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid about-div">
+            <div className="row">
+                <div className="col-md-12">
+                    <p className="aboutbody">
+                        Hello there ! My name is Adisa and I am a Digital Enthusiast and passionate FrontEnd Web developer.
+                        <br></br> My interest in web development started back in 2020 when I wondered how the internet worked and decided to take a leap of faith.
+                        <br></br> My main aim is building accessible, blazing fast & mobile responsive websites for a variety of client.
+                    </p>
+                </div>
+            </div>
+            </div>
+            <div className="container-fluid about-div">
+                <div className="row">
+                    <div className="col-md-12">
+                        <p className="aboutme">MY SKILLS</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        {/* <section>
+            <div className="row">
+             <div className="col-lg-12">
+             <hr className="line" />
+        </div>
+            </div>
+        </section> */}
 
         </>
  )
