f8638cfed6b72f8fd1d1d125d0a163ce948dfa85
Gizzhy <gizzhyakinz@gmail.com>
2023-03-17 15:13:47 +0100

    new com

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export const Navbar = () => {
         <div class="container-fluid">
         <a className="navbar-brand" href="/#"><img className="logo" src={logo} alt="" /></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon navbar-dark"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
